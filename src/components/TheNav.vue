999394dfc0b3bcffd4a69e715b09217e00195b63
Your Name <you@example.com>
2023-07-13 22:03:54 +0300

    NavItem Component

---
+++ src/components/TheNav.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import NavItem from './NavItem.vue'
+const navItems = {
+  timeline: ClockIcon,
+  activities: ListBulletIcon,
+  progress: ChartBarIcon
+}
+</script>
+<template>
+  <nav class="text-3x1 sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <NavItem v-for="(icon, page) in navItems" :key="page" :href="`#${page}`">
+        <component :is="icon" class="h-6 w-6" />{{ page }}
+      </NavItem>
+    </ul>
+  </nav>
+</template>
