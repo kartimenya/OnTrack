50d28a124239a3fee0fbe96b1d74f07e93f4f8ff
Your Name <you@example.com>
2023-08-10 19:46:05 +0300

    My router

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,17 +1,11 @@
 <script setup>
 import TheLogo from './TheLogo.vue'
 import TheHeaderProgress from './TheHeaderProgress.vue'
-import { isPageValid } from '../validators'
-import { PAGE_PROGRESS, PAGE_TIMELINE } from '../constans'
-
-const emit = defineEmits({
-  navigate: isPageValid
-})
 </script>
 
 <template>
   <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
-    <TheLogo @:click="emit('navigate', PAGE_TIMELINE)" />
-    <TheHeaderProgress @click="emit('navigate', PAGE_PROGRESS)" />
+    <TheLogo />
+    <TheHeaderProgress />
   </header>
 </template>
